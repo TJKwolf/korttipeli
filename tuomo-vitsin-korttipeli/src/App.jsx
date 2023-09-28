bc2adfafe7c1bf80853d15598847ebfd78ddb3b2
Your Name <you@example.com>
2023-09-28 19:26:47 +0300

    tehtiin create card

--- tuomo-vitsin-korttipeli/src/App.jsx
+++ tuomo-vitsin-korttipeli/src/App.jsx
@@ -1,45 +1,63 @@
 import Card from "./componets/Card"
 import'./App.css'
+import {useState} from 'react'
+
+const getRandomInt = (min, max)=> Math.floor(Math.random() * (max - min +1) + min);
+
 const playerCard = {
   image: "http://placekitten.com/130/110",
-  stats: [{name: 'cuteness',value: 30},
-          {name: 'speed', value: 70},
-          {name: 'power', value: 39}]
+  stats: [{name: 'cuteness',value: getRandomInt(1, 999)},
+          {name: 'speed', value: getRandomInt(1, 999)},
+          {name: 'power', value: getRandomInt(1, 999)}]
 }
 
 const opponentcard ={
   image: "http://placekitten.com/130/100?image=2",
-  stats: [{name: 'cuteness',value: 40},
-          {name: 'speed', value: 20},
-          {name: 'power', value: 90}]
+  stats: [{name: 'cuteness',value: getRandomInt(1, 999)},
+          {name: 'speed', value: getRandomInt(1, 999)},
+          {name: 'power', value: getRandomInt(1, 999)}]
 }
 
+const createCard = index =>({
+    image: "http://placekitten.com/130/100?=" + index,
+    stats: [{name: 'cuteness',value: getRandomInt(1, 999)},
+            {name: 'speed', value: getRandomInt(1, 999)},
+            {name: 'power', value: getRandomInt(1, 999)}
+          ],
+  id: crupto.randomUUID()
+})
+
 export default  function App(){
+
+  const[result,setResult] = useState('');
   
   function comperCards(){
     const playerStats = playerCard.stats[0];
     const opponentStats = opponentcard.stats[0];
-    let result = '';
+
 
     if(playerStats.value === opponentStats.value){
-      result = "draw";
+      setResult('draw');
     }
     else if(playerStats.value > opponentStats.value){
-      result = "win";
+      setResult('win');
     }
     else{
-      result = "lose";
+      setResult('lose');
     }
-    console.log(result);
+
   }
-  
   return(
     <>
-      <h1>Hello world</h1>
-      <Card card={playerCard}/>
-      <button type='button' onClick={comperCards}>play</button>
-      <Card card={opponentcard}/>
-
+      <h1>Cat_game</h1>
+       <div className='game'>
+         <Card card={playerCard}/>
+          <div className='center-area'>
+            <p>{result ||'Press The Button'}</p>
+            <button type='button' onClick={comperCards}>play</button>
+          </div>
+         <Card card={opponentcard}/>
+       </div>
     </>
  
   );
